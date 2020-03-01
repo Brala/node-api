79c8f5b14fe24f9d3738daabf9a6405e00d934b2
Brala <elobralu@gmail.com>
2020-03-01 16:44:28 +0100

    all enduser-request data parsed

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
 const server = http.createServer(function(req,res){
@@ -17,11 +18,30 @@ const server = http.createServer(function(req,res){
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g,'');
     
-    // Send the response
-    res.end('hello World\n');
+    // Get the query string as an object
+    const queryStringObject = parsedUrl.query;
+    
+    // Get the HTTP Method
+    const method = req.method.toLowerCase();
+    
+    // Get the headers object
+    const headers = req.headers;
 
-    // Log the request path
-    console.log('Request recieved on path: '+trimmedPath)
+    // Get the payload, if any
+    const decoder = new StringDecoder("utf-8")
+    let buffer = '';
+    req.on('data',function(data){
+        buffer += decoder.write(data);
+    })
+    req.on('end',function(){
+        buffer += decoder.end();
+
+        // Send the response
+        res.end('hello World\n');
+
+        // Log the request path
+        console.log('Request recieved with: ', buffer)
+    })
 
 });
 
